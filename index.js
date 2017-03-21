63fc85e6e0232aa43840ff2aea28b5801fed3db8
Tiago Silva Pereira Rodrigues <tiagosilvapereira3@gmail.com>
2017-03-21 02:01:05 -0300

    Improvements to agilize Plugs creation

--- index.js
+++ index.js
@@ -35,14 +35,115 @@ module.exports = {
         return (field.component == 'file-image');
     },
 
+    getFieldHTMLTag: function(field) {
+        if(this.isTextField(field) || this.isNumberField(field) || this.isTextAreaField(field)){
+            return field.component;
+        }else
+        if(this.isFileField(field) || this.isFileImageField(field)){
+            return 'file';
+        }
+    },
+
     /* Code Helpers */
+    getTemplateCode: function(content, templateName, manipulatorCallback) {
+        let startTemplate = 'PwTemplate:' + templateName,
+            endTemplate = 'PwEndTemplate',
+            template = this.getCodeBetween(content, startTemplate, endTemplate);
+
+        if(manipulatorCallback) template = manipulatorCallback(template);
+
+        return template;
+    },
+
+    setTemplateSectionCode: function(content, templateSectionName, replacement) {
+        let startTemplateSection = 'PwTemplateSection:' + templateSectionName,
+            endTemplateSection = 'PwEndTemplateSection';
+
+        return this.replaceCodeFromTo(content, startTemplateSection, endTemplateSection, replacement);
+    },
+
+    removeTemplates: function(content, templates) {
+        templates.forEach((templateName) => {
+            let startTemplate = 'PwTemplate:' + templateName,
+                endTemplate = 'PwEndTemplate';
+
+            content = this.replaceCodeFromTo(content, startTemplate, endTemplate, '');
+        });
+
+        return content;
+    },
+
+    getCodeBetween: function(content, word1, word2) {
+        var re = new RegExp("(?:"+word1+")([^]*?)(?:"+word2+")", "ig"),
+            reExec = re.exec(content);
+        
+        if (reExec && reExec.length > 1) {
+            return reExec[1];
+        }
+        
+        return '';
+    },
+
     replaceCode: function(content, variable, replacement) {
         var re = new RegExp('{%' + variable + '%}', 'g');
         return content.replace(re, replacement);
     },
 
+    replaceCodeFromTo: function(content, from, to, replacement) {
+        var re = new RegExp("(?:"+from+")([^]*?)(?:"+to+")", "ig");
+        return content.replace(re, replacement);
+    },
+
+    replaceObjectDefaultCode: function(code, object) {
+        code = this.replaceCode(code, 'object', object.name_singular);
+        code = this.replaceCode(code, 'Object', this.capitalize(object.name_singular));
+        code = this.replaceCode(code, 'objects', object.name);
+        code = this.replaceCode(code, 'Objects', this.capitalize(object.name));
+        return code;
+    },
+
+    replaceFieldDefaultCode: function(code, field) {
+        code = this.replaceCode(code, 'field', field.name);
+        code = this.replaceCode(code, 'Field', this.capitalize(field.name));
+        code = this.replaceCode(code, 'fieldType', this.getFieldHTMLTag(field));
+        return code;
+    },
+
+    forEachCode: function(content, blockName, items, manipulatorCallback, blockDefaultReplacement) {
+        let startEachBlock = 'PwEach:' + blockName,
+            endEachBlock = 'PwEndEach',
+            eachBlock = this.getCodeBetween(content, startEachBlock, endEachBlock),
+            code = '', actualBlockCode = '';
+
+        items.forEach((item) => {
+            actualBlockCode = eachBlock;
+
+            if(!blockDefaultReplacement)
+                actualBlockCode = this.replaceObjectDefaultCode(eachBlock, item);
+
+            if(manipulatorCallback) 
+                actualBlockCode = manipulatorCallback(actualBlockCode, item);
+
+            code += actualBlockCode.replace(/\s+$/g, ''); // Replace last \n - New Line
+        });
+
+        content = this.replaceCodeFromTo(content, startEachBlock, endEachBlock, code);
+
+        return content;
+    },
+
     getReplacementCode: function(variable) {
         return new RegExp('{%' + variable + '%}', 'g');
+    },
+
+    objectFieldsAsString: function(object) {
+        var fields = [];
+        
+        object.structure.forEach(function(field){
+            fields.push('\'' + field.name + '\'');
+        });
+
+        return fields.join(',');
     },
 
     capitalize: function(string) {
@@ -89,6 +190,13 @@ module.exports = {
             if(manipulatorCallback) 
                 textData = manipulatorCallback(textData);
 
+            this.writeFile(destFile, textData);
+        });
+    },
+
+    fileFromBaseFileWithDefault: function(baseFile, destFile, object) {
+        this.readFile(baseFile, (textData) => {
+            textData = this.replaceObjectDefaultCode(textData, object);
             this.writeFile(destFile, textData);
         });
     },
